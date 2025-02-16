c63ddc6ac517c09fac92065be29b223456fd69b3
jerkvania <jerkvania@gmail.com>
2025-02-16 15:22:29 +0200

    Update que.py

--- que.py
+++ que.py
@@ -260,6 +260,10 @@ async def main():
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main())
+        loop = asyncio.get_event_loop()
+        if loop.is_running():
+            loop.create_task(main())
+        else:
+            loop.run_until_complete(main())
     except Exception as e:
         logger.error(f"Ошибка при запуске бота: {e}")
